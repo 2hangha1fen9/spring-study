29c25317ddc42c6d30c71966b9ed0668adc67a4c
zhang <zhang1287609286@qq.com>
2023-06-14 23:31:19 +0800

    commit

--- spring-first/src/main/java/cloud/zhfsmy/HelloWorld.java
+++ spring-first/src/main/java/cloud/zhfsmy/HelloWorld.java
@@ -1,6 +1,10 @@
 package cloud.zhfsmy;
 
 public class HelloWorld {
+    public HelloWorld() {
+        System.out.println("类初始化");
+    }
+
     public void sayHello() {
         System.out.println("Hello Spring!");
     }
--- spring-first/src/test/java/HellWordTest.java
+++ spring-first/src/test/java/HellWordTest.java
@@ -1,13 +1,18 @@
 import cloud.zhfsmy.HelloWorld;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HellWordTest {
+    private Logger logger = LoggerFactory.getLogger(HellWordTest.class);
+
     @Test
     public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         HelloWorld main = (HelloWorld) context.getBean("helloWorld");
         main.sayHello();
+        logger.info("测试log42j日志框架");
     }
 }
